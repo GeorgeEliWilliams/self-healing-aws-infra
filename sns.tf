efd9947f0df6e96149c2aaf7ab3f3bf60b90fdcf
GeorgeEliWilliams <williamsgeorge950@gmail.com>
2026-09-13 13:07:28 +0000

    add SNS topic and email subscription for cluster alerts

---
+++ sns.tf
@@ -0,0 +1,9 @@
+resource "aws_sns_topic" "alerts" {
+  name = "k3s-clusteralerts"
+}
+
+resource "aws_sns_topic_subscription" "alerts_email" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = var.alerts_email
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "my_ip" {
   description = "public IP for SSH/K8s API access"
   type        = string
 }
+
+variable "alerts_email" {
+  description = "Email address to receive SNS cluster alerts"
+  type        = string
+}
